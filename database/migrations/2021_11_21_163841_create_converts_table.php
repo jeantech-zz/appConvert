773d0dadf1f637a47f8705e68fc9ec5bca223804
Jennifer <jeante05@gmail.com>
2021-11-21 12:08:30 -0500

    create convert

---
+++ database/migrations/2021_11_21_163841_create_converts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('converts', function (Blueprint $table) {
+            $table->id();
+            $table->String('currency');
+            $table->float('exchange', 8, 2);
+            $table->date('currency_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('converts');
+    }
+}
